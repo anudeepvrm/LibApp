8c15e2d6bef822da6b716c61e63eac659b8d9d7b
anudeep <vdatla@ncsu.edu>
2016-09-20 19:06:43 -0400

    adding main controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160918045958) do
+ActiveRecord::Schema.define(version: 20160920224642) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "username"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
     t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
