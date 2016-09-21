c960cd00641348fae5667d1dc1be4ffbeb4980d7
anudeep <vdatla@ncsu.edu>
2016-09-20 20:56:21 -0400

    removing scrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'main#index'
   resources :users
+  resources :admins
+
+  get '/user_login', to: 'users#login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
