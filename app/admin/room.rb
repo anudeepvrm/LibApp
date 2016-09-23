ab743b53b59324814efe1d38e89bce81249e8539
Jitin Kumar <jkumar3@ncsu.edu>
2016-09-22 22:07:27 -0400

    Stable Base v1.1

--- app/admin/room.rb
+++ app/admin/room.rb
@@ -1,17 +1,10 @@
 ActiveAdmin.register Room do
+  permit_params :building, :size, :room_num
+  index do
+    column :building
+    column "Sitting Capacity", :size
+    column "Room Number", :room_num
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
-
+  end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_filter do
+    resource = controller_path.singularize.gsub('/', '_').to_sym
+    method = "#{resource}_params"
+    params[resource] &&= send(method) if respond_to?(method, true)
+  end
   protect_from_forgery with: :exception
+
 end
