88a8af171e659b88eaca2836d66139ffdec45696
root <root@localhost.localdomain>
2016-09-24 19:43:11 -0400

    try

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,6 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
+  config.default_namespace = :admin_ui
   config.site_title = "Libapp"
 
   # Set the link url for the title. For example, to take
