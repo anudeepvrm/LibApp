e5ede976f2adc9140aa46a15067d8bfe871e3050
anudeep <vdatla@ncsu.edu>
2016-09-24 23:42:30 -0400

    breakpoint

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Libapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.time_zone = 'Riga'
-    config.active_record.default_timezone = 'utc'
+    config.time_zone = 'Eastern Time (US & Canada)'
+    config.active_record.default_timezone = 'EST'
   end
 end
