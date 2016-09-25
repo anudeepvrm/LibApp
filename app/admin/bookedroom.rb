67d27fd1ac71ab58a501ee83975d85a94b2e883c
Jitin Kumar <jkumar3@ncsu.edu>
2016-09-24 22:39:40 -0400

    bookedroom activeadmin added

--- app/admin/bookedroom.rb
+++ app/admin/bookedroom.rb
@@ -1,17 +1,27 @@
 ActiveAdmin.register Bookedroom do
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  permit_params :user, :room, :user_id, :room_id, :status, :booking_time, :id
 
+  index do
+    column :id
+    column :booking_time
+    column :room_id do |room_id|
+      if room_id.present?
+        link_to room_id.room_id
+      else
+        status_tag('Empty')
+      end
+    end
+
+    column :user_id do |user_num|
+      if user_num.present?
+        link_to user_num.user_id
+      else
+        status_tag('Empty')
+      end
+    end
+
+  end
+  
 
 end
--- app/models/bookedroom.rb
+++ app/models/bookedroom.rb
@@ -2,4 +2,6 @@ class Bookedroom < ApplicationRecord
   has_one :user
   has_one :room
   attr_accessor :status
+  attr_accessor :user
+  attr_accessor :room
 end
