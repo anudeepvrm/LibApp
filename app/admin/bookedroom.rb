e00f8ba5c057ecf72754398427d13cbe9e84b50b
Jitin Kumar <jkumar3@ncsu.edu>
2016-10-05 21:32:50 -0400

    BookedRoom Updates after updates

--- app/admin/bookedroom.rb
+++ app/admin/bookedroom.rb
@@ -21,6 +21,17 @@ ActiveAdmin.register Bookedroom do
     end
   end
 
+  form do |f|
+    f.inputs "Booking Details" do
+      f.input :room_id, label: 'Room Number', :as => :select, :collection => Room.all.collect{|room|[room.roomno]}, include_blank: false
+      f.input :user_id, label: 'User', :as => :select, :collection => User.all, include_blank: false
+      f.input :status, label: 'Status', :as => :select, :collection => ["Booked","Available"], include_blank: false
+      f.input :booking_time
+    end
+    f.actions
+  end
+
+
 
 
 end
